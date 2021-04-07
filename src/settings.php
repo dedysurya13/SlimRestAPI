c65625b754d3b0924c21c07f138183235636cc4f
Dedy Surya Prihantoro <44486274+dedysurya13@users.noreply.github.com>
2021-04-07 14:10:30 +0700

    db

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,14 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // Database Settings
+        'db' => [
+            'server' => 'DESKTOP-ACIFTV2\SQLEXPRESS',
+            'user' => 'sa',
+            'pass' => 'admin1',
+            'database' => 'asetrsuic',
+            'driver' => 'sqlsrv'
+        ]
     ],
 ];
