1aa05709532894474d24776c0c943de40370b91e
Dedy Surya Prihantoro <44486274+dedysurya13@users.noreply.github.com>
2021-04-07 14:09:38 +0700

    add db dependencies

--- src/dependencies.php
+++ src/dependencies.php
@@ -19,4 +19,14 @@ return function (App $app) {
         $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
         return $logger;
     };
+
+    //database
+    $container['db'] = function($c){
+        $settings = $c->get('settings')['db'];
+        $server = $settings['driver'].":server=".$settings['server'].";database=".$settings['database'];
+        $conn = new PDO($server, $settings["user"], $settings["pass"]);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $conn;
+    };
 };
